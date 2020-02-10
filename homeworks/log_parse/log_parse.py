0c0eb479f3722b5e1e241c90520a5c7342e8e7dc
curlymario <maxim@sivokon.me>
2020-02-10 10:23:48 +0300

    hw2 filtering loglines and saving into namedtuple

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -1,5 +1,8 @@
 # -*- encoding: utf-8 -*-
 
+from collections import namedtuple
+import re
+
 def parse(
     ignore_files=False,
     ignore_urls=[],
@@ -9,7 +12,23 @@ def parse(
     ignore_www=False,
     slow_queries=False
 ):
-    '''
-        ТУТ ДОЛЖЕН БЫТЬ ВАШ КОД
-    '''
+    logfile = open('log.log', 'r')
+    Logline = namedtuple('Logline', [
+        'request_date',
+        'request_time',
+        'request_type',
+        'request',
+        'protocol',
+        'response_code',
+        'response_time'
+    ])
+    logstart = re.compile('\[[0-2][0-9]/[A-Za-z]{3}/[1-2][0-9]{3} [0-2][0-9]:[0-5][0-9]:[0-9][0-9]\]')
+    parsed_log = set()
+    for line in logfile.readlines():
+        if logstart.match(line[0:22]):
+            line_content = Logline(*line.replace('"','').split())
+            parsed_log.add(line_content)
     return []
+
+if __name__ == '__main__':
+    parse()
