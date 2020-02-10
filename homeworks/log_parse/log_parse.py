1f9639c54a15a16d437504b3e5eef4e5c6373e33
curlymario <maxim@sivokon.me>
2020-02-10 11:19:34 +0300

    refactoring more logical (I guess) var names empty lines by PEP guidelines

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -3,17 +3,18 @@
 from collections import namedtuple
 import re
 
+
 def parse(
-    ignore_files=False,
-    ignore_urls=[],
-    start_at=None,
-    stop_at=None,
-    request_type=None,
-    ignore_www=False,
-    slow_queries=False
+        ignore_files=False,
+        ignore_urls=[],
+        start_at=None,
+        stop_at=None,
+        request_type=None,
+        ignore_www=False,
+        slow_queries=False
 ):
-    logfile = open('log.log', 'r')
-    Logline = namedtuple('Logline', [
+    log_file = open('log.log', 'r')
+    Log_entry = namedtuple('Log_entry', [
         'request_date',
         'request_time',
         'request_type',
@@ -22,13 +23,14 @@ def parse(
         'response_code',
         'response_time'
     ])
-    logstart = re.compile('\[[0-2][0-9]/[A-Za-z]{3}/[1-2][0-9]{3} [0-2][0-9]:[0-5][0-9]:[0-9][0-9]\]')
+    log_start = re.compile('\[[0-2][0-9]/[A-Za-z]{3}/[1-2][0-9]{3} [0-2][0-9]:[0-5][0-9]:[0-9][0-9]\]')
     parsed_log = set()
-    for line in logfile.readlines():
-        if logstart.match(line[0:22]):
-            line_content = Logline(*line.replace('"','').split())
-            parsed_log.add(line_content)
+    for string in log_file.readlines():
+        if log_start.match(string[0:22]):
+            entry = Log_entry(*string.replace('"', '').split())
+            parsed_log.add(entry)
     return []
+
 
 if __name__ == '__main__':
     parse()
