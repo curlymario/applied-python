fa1ad4dd973c8a4911d28191de976ceb255a25cc
curlymario <maxim@sivokon.me>
2020-02-13 10:59:34 +0300

    refactoring and type error fixes

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -8,10 +8,32 @@ import datetime
 class Log_entry(NamedTuple):
     request_date: str
     request_type: str
-    request: list
+    request: tuple
     protocol: str
     response_code: int
     response_time: int
+
+
+def read_log(log_file):
+    log_start = re.compile('\[[0-2][0-9]/[A-Za-z]{3}/[1-2][0-9]{3} [0-2][0-9]:[0-5][0-9]:[0-9][0-9]\]')
+    schema_pattern = re.compile('^.*:(?=//)')
+    parsed_log = []
+    for string in log_file.readlines():
+        if log_start.match(string[0:22]):
+            _date, _time, _type, _request, _protocol, _r_code, _r_time = string.replace('"', '').split()
+            _date = _date.replace('[', '')
+            _time = _time.replace(']', '')
+            _schema = schema_pattern.search(_request)
+            entry = Log_entry(
+                request_date=' '.join((_date, _time)),
+                request_type=_type,
+                request=(_schema, ),
+                protocol=_protocol,
+                response_code=int(_r_code),
+                response_time=int(_r_time)
+            )
+            parsed_log.append(entry)
+    return parsed_log
 
 
 def parse(
@@ -27,28 +49,6 @@ def parse(
     log = read_log(log_file)
 
     return []
-
-
-def read_log(log_file):
-    log_start = re.compile('\[[0-2][0-9]/[A-Za-z]{3}/[1-2][0-9]{3} [0-2][0-9]:[0-5][0-9]:[0-9][0-9]\]')
-    schema_pattern = re.compile('^.*:(?=//)')
-    parsed_log = set()
-    for string in log_file.readlines():
-        if log_start.match(string[0:22]):
-            _date, _time, _type, _request, _protocol, _r_code, _r_time = [*string.replace('"', '').split()]
-            _date = _date.replace('[', '')
-            _time = _time.replace(']', '')
-            _schema = schema_pattern.search(_request)
-            entry = Log_entry(
-                request_date=(' ').join(_date, _time),
-                request_type=type,
-                request=[_schema, ],
-                protocol=_protocol,
-                response_code=int(_r_code),
-                response_time=int(_r_time)
-            )
-            parsed_log.add(entry)
-    return parsed_log
 
 
 if __name__ == '__main__':
