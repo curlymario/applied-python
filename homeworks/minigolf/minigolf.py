511f068aae1e033c8dbe902fac8dd1e5ad26197e
curlymario <maxim@sivokon.me>
2020-02-19 18:54:00 +0300

    minigolf - HitsMatch raw game logic

--- homeworks/minigolf/minigolf.py
+++ homeworks/minigolf/minigolf.py
@@ -14,13 +14,22 @@ class Match:
 
         self._finished = False
         self._current_hole = 0
+        self._current_round = 0
         self._current_player = 0
+        self._tick = 0
         for player in self._players:
-            player.hits = 0
+            player.score = 0
 
         self._table = [[player.name for player in players]]+[[None]*len(players) for _ in range(holes)]
         self._winners = set()
 
+    def _wrap_players_list(self):
+        while self._current_player >= len(self._players):
+            self._current_player -= len(self._players)
+
+    def _next_player(self):
+        self._current_player += 1
+        self._wrap_players_list()
 
     def hit(self, success=False):
         """
@@ -30,13 +39,11 @@ class Match:
         """
         if self.finished:
             raise RuntimeError('Матч завершён')
-
         self._hit(success)
 
         self._current_player += 1
         if self._current_player == len(self._players):
             self._current_player = 0
-
 
     @property
     def finished(self):
@@ -74,9 +81,60 @@ class HitsMatch(Match):
     Каждый игрок получает столько очков, сколько ударов он потратил (соответственно, чем меньше очков, тем лучше).
     Десятый удар защитывается автоматически, игрок получает 10 очков, удар не требуется.
     """
-    def _hit(self, success):
-        pass
 
+    def __init__(self, holes, players):
+        Match.__init__(self, holes, players)
+        self._playing = set(players)
+
+    def _hit(self, success):
+        self._tick += 1
+        print(self._tick)
+
+        player = self._players[self._current_player]
+        while self._playing and player not in self._playing:
+            print('skip player ' + str(self._current_player) + ' ' + player.name)
+            self._next_player()
+            if self._tick >= len(self._players):
+                self._current_round += 1
+                self._tick = 0
+                print('------\nnew round ' + str(self._current_round) + ' in hole ' + str(self._current_hole) + '\n')
+            self._tick += 1
+            print(self._tick)
+            player = self._players[self._current_player]
+
+        print(str(self._current_player) + ' ' + player.name + ' hits')
+        if success:
+            player.success = True
+            player.score += 1
+            self._table[self._current_hole + 1][self._current_player] = player.score
+            self._playing.remove(player)
+            print(str(self._current_player) + ' ' + player.name + ' has finished succesfully\n')
+
+        else:
+            print(str(self._current_player) + ' ' + player.name + ' missed\n')
+            if self._current_round == 8:
+                self._table[self._current_hole + 1][self._current_player] = 10
+                self._playing.remove(player)
+                print(str(self._current_player) + ' ' + player.name + ' could not finish\n')
+            else:
+                player.score += 1
+
+        if not self._playing:
+            self._tick = 0
+            self._current_round = 0
+            self._current_hole += 1
+            if self._current_hole > self._holes:
+                self._finished = True
+            else:
+                self._current_player = self._current_hole
+                self._wrap_players_list()
+                self._playing = set(self._players)
+                print('===========================\nNew hole!\n' + str(self._current_player) + ' ' + self._players[self._current_player].name + ' starts \n')
+
+        if self._tick >= len(self._players):
+            self._tick = 0
+            self._current_round += 1
+            print('------\nnew round ' + str(self._current_round) + ' in hole ' + str(self._current_hole) + '\n')
 
 class HolesMatch(Match):
     """
