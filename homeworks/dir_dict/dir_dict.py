4b68fcaf819f14188fba4d04b68659e261d3db02
curlymario <maxim@sivokon.me>
2020-03-18 14:25:47 +0300

    dir_dict — interface

---
+++ homeworks/dir_dict/dir_dict.py
@@ -0,0 +1,69 @@
+class DirDict(dict):
+    def __init__(self, dir):
+        pass
+
+    def __contains__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def __setitem__(self):
+        pass
+
+    def __delitem__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def __ne__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def copy(self):
+        pass
+
+    def fromkeys(self):
+        pass
+
+    def get(self):
+        pass
+
+    def items(self):
+        pass
+
+    def keys(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def popitem(self):
+        pass
+
+    def setdefault(self):
+        pass
+
+    def update(self):
+        pass
+
+    def values(self):
+        pass
