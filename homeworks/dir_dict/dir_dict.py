f8bf22444be7a07994fa7093939e4670e1db792a
curlymario <maxim@sivokon.me>
2020-03-20 14:58:41 +0300

    dir_dict — most methods (using pathlib)

--- homeworks/dir_dict/dir_dict.py
+++ homeworks/dir_dict/dir_dict.py
@@ -1,76 +1,101 @@
+import pathlib
+
+
 class DirDict():
     def __init__(self, working_dir, *args, **kwargs):
-        self._dir = working_dir
+        self._dir = pathlib.Path(working_dir)
+        self._dir.mkdir(parents=True, exist_ok=True) #TODO: обработать случай, когда директория существует
         self.update(*args, **kwargs)
 
+    def __contains__(self, key):
+        return self._dir.joinpath(key).exists()
+
     def __getitem__(self, key):
-        file = open('/'.join((self._dir, key)), 'r')
-        value = file.read()
-        file.close()
-        return value
+        if self.__contains__(key):
+            with self._dir.joinpath(key).open('r') as file:
+                value = file.read_text()
+            return value
+        else:
+            raise KeyError
 
     def __setitem__(self, key, value):
-        file = open('/'.join((self._dir, key)), 'w')
-        file.write(value)
-        file.close()
+        path = self._dir.joinpath(key)
+        if not path.exists():
+            path.touch()
+        with path.open('w') as file:
+            file.write_text(value)
 
     def __delitem__(self, key):
-        pass
-
-    def __contains__(self, value):
-        pass
+        if self.__contains__(key):
+            path = self._dir.joinpath(key)
+            path.unlink()
+        else:
+            raise KeyError
 
     def __iter__(self):
-        pass
+        return self._dir.iterdir()
 
     def __len__(self):
-        pass
+        return len(list(self.__iter__()))
 
-    def __eq__(self, other):
-        pass
-
-    def __ne__(self, other):
-        pass
-
-    def __repr__(self):
-        pass
-
-    def __str__(self):
-        pass
-
-    def __hash__(self):
-        pass
+    # def __eq__(self, other):
+    #     pass
+    #
+    # def __ne__(self, other):
+    #     pass
+    #
+    # def __repr__(self):
+    #     pass
+    #
+    # def __str__(self):
+    #     pass
+    #
+    # def __hash__(self):
+    #     pass
 
     def clear(self):
-        pass
+        for file in self.__iter__():
+            file.unlink()
 
-    def copy(self):
-        pass
+    # def copy(self):
+    #     return
 
-    def fromkeys(self):
-        pass
+    # def fromkeys(self):
+    #     pass
 
-    def get(self, key):
-        pass
+    def get(self, key, default=None):
+        if self.__contains__(key):
+            return self.__getitem__(key)
+        else:
+            return default
 
-    def items(self):
-        pass
+    def _iter_keys(self):
+        for file in self.__iter__():
+            yield file.name
 
     def keys(self):
-        pass
+        return [key for key in self._iter_keys()]
+
+    def items(self):
+        return [(key, self.__getitem__(key)) for key in self._iter_keys()]
+
+    def values(self):
+        return [self.__getitem__(key) for key in self._iter_keys()]
 
     def pop(self, key):
-        pass
+        value = self.get(key, default=None)
+        self.__delitem__(key)
+        return value
 
-    def popitem(self):
-        pass
+    def popitem(self, key):
+        value = self.get(key, default=None)
+        self.__delitem__(key)
+        return key, value
 
     def setdefault(self, key, default=None):
-        pass
+        if not self.__contains__(key):
+            self.__setitem__(key, default)
 
     def update(self, *args, **kwargs):
         for key, value in dict(*args, **kwargs).items():
             self[key] = value
-
-    def values(self):
-        pass
